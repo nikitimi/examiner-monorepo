e69dc633058362dfbab40d2e4f4a20a56b49d269
Gian Carlo Carranza <giancarloacarranza@gmail.com>
2024-12-08 01:12:47 +0800

    feat: removed default nextjs landing page.

---
+++ packages/nextjs/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <p>Website is still under construction... ⚠</p>
+    </footer>
+  );
+}
